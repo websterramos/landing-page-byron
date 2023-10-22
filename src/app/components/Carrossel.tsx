81899216b5ed9f72e69263dd61ffb96a179e86de
FQuental <felipedoquental@gmail.com>
2023-10-22 12:37:09 -0300

    novas funcionalidades no carrossel

--- src/app/components/Carrossel.tsx
+++ src/app/components/Carrossel.tsx
@@ -1,19 +1,28 @@
 'use client';
 import React, { useState } from 'react';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs'
+import { RxDotFilled } from 'react-icons/rx';
 
 export default function Carrossel() {
     const slides = [
         {
             url: "/assets/img/bird1.png",
+            titulo: "exemplo 1",
             texto: "texto 1"
         },
         {
             url: "/assets/img/bird2.png",
+            titulo: "exemplo 2",
             texto: "texto 2"
         },
         {
             url: "/assets/img/bird3.png",
+            titulo: "exemplo 3",
+            texto: "texto 3"
+        },
+        {
+            url: "/assets/img/bird3.png",
+            titulo: "exemplo 3",
             texto: "texto 3"
         },
     ];
@@ -32,10 +41,17 @@ export default function Carrossel() {
         setCurrentIndex(newIndex);
     };
 
+    const goToSlide = (slideIndex) => {
+        setCurrentIndex(slideIndex);
+    };
+
     return (
 
-        <div className="max-w-[1400px] h-[780px] w-full n-auto py-16 px-4 relative group">
-            <div style={{ backgroundImage: `url(${slides[currentIndex].url})` }} className="w-ful h-full rounded-2xl bg-center bg-cover duration-500"> {slides[currentIndex].texto}</div>
+        <div className=" h-[780px] w-full n-auto py-16 px-4 relative group">
+            <div style={{ backgroundImage: `url(${slides[currentIndex].url})` }} className=" w-full h-full rounded-2xl bg-center lg:bg-cover bg-contain bg-no-repeat duration-500">
+                <h1 className='text-7xl'>{slides[currentIndex].titulo}</h1>
+                <p>{slides[currentIndex].texto}</p>
+            </div>
 
             {/* {left arrow} */}
             <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
@@ -46,6 +62,17 @@ export default function Carrossel() {
                 <BsChevronCompactRight onClick={nextSlide} size={30} />
             </div>
 
+            <div className='flex top-4 justify-center py-2'>
+                {slides.map((slide, slideIndex) => (
+                    <div
+                        key={slideIndex}
+                        onClick={() => goToSlide(slideIndex)}
+                        className='text-2xl cursor-pointer'
+                    >
+                        <RxDotFilled />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 
