a3490e527c8a79b293c1140b3965fe4bb4932d93
FQuental <felipedoquental@gmail.com>
2023-10-23 09:51:23 -0300

    correção do texto do carrossel atravessando o header

--- src/components/Carrossel.tsx
+++ src/components/Carrossel.tsx
@@ -49,7 +49,7 @@ export default function Carrossel() {
       </div>
 
       <div className=" relative rounded-2xl bg-cover bg-center bg-no-repeat duration-500 lg:bg-contain">
-        <div className="pointer-events-none absolute inset-0 z-10 flex flex-col items-center justify-end py-10">
+        <div className="pointer-events-none absolute inset-0 z-9 flex flex-col items-center justify-end py-10">
           <div className="rounded-xl bg-black/30 px-1 py-0.5 text-center backdrop-blur backdrop-contrast-200">
             <h1 className="text-3xl text-white">
               {slides[currentIndex].titulo}
