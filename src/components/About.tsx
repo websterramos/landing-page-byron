2b90ca0d2f0ee8baa3286c09de242399d6291d60
“websterramos” <websterramos43@gmail.com>
2024-01-02 17:09:25 -0300

    Aplicando o estilo padronizado nas seções de sobre e contato

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,44 +4,44 @@ const About = () => {
   return (
     <section
       id="sobre"
-      className="flex h-full items-center justify-center bg-white px-4 py-8 text-[#012130] md:px-40 md:py-24"
+      className="flex h-full items-center justify-center bg-white px-4 py-8 text-[#012130] lg:px-40 lg:py-24"
     >
       <div id="sobre-div" className="max-w-7xl">
         <div
           id="sobre-content"
-          className="flex flex-col items-center justify-center gap-6 md:flex-row md:gap-24 md:px-0"
+          className="flex flex-col items-center justify-center gap-6 lg:flex-row lg:gap-24 lg:px-0"
         >
           <div
             id="sobre-foto"
-            className="flex flex-col items-center justify-center gap-4 px-2 md:max-w-lg md:flex-row md:gap-5 md:px-0"
+            className="flex flex-col items-center justify-center gap-4 px-2 lg:max-w-lg lg:flex-row lg:gap-5 lg:px-0"
           >
-            <div className="flex flex-col gap-4 md:gap-[22px]">
+            <div className="flex flex-col gap-4 lg:gap-[22px]">
               <img
-                className="max-h-[190px] max-w-[312px] rounded-lg md:max-h-[316px] md:max-w-[236px]"
-                src="img/Pic1.png"
+                className="h-[190px] w-[312px] overflow-hidden rounded-lg object-cover object-top lg:h-[316px] lg:w-[236px] lg:object-center"
+                src="img/about-pic-1.jpg"
               />
               <img
-                className="max-h-[190px] max-w-[312px] rounded-lg md:max-h-[316px] md:max-w-[236px]"
-                src="img/Pic3.png"
+                className="h-[190px] w-[312px] overflow-hidden rounded-lg object-cover object-center lg:h-[316px] lg:w-[236px] lg:object-center"
+                src="img/about-pic-3.jpg"
               />
             </div>
             <img
-              className="max-h-[190px] max-w-[312px] rounded-lg md:max-h-[316px] md:max-w-[236px]"
-              src="img/Pic2.png"
+              className="h-[190px] w-[312px] overflow-hidden rounded-lg object-cover object-center lg:h-[316px] lg:w-[236px] lg:object-left"
+              src="img/about-pic-2.jpg"
             />
           </div>
           <div
             id="sobre-texto"
-            className="flex w-full flex-col items-start justify-center gap-4 md:max-h-[580px] md:max-w-lg"
+            className="flex w-full flex-col items-start justify-center gap-4 lg:max-h-[580px] lg:max-w-lg"
           >
-            <h4 className="text-2xl uppercase leading-8 text-[#0B719D] md:text-3xl md:leading-10">
+            <h5 className="text-2xl uppercase leading-8 text-[#0B719D] lg:text-3xl lg:leading-10">
               Sobre nós
-            </h4>
-            <div className="flex flex-col justify-center gap-4 md:gap-2">
-              <h3 className="text-3xl leading-[49px] md:text-5xl">
+            </h5>
+            <div className="flex flex-col justify-center gap-4 lg:gap-2">
+              <h4 className="text-4xl leading-[49px] lg:text-5xl">
                 Make your customers happy by giving services.
-              </h3>
-              <p className="font-light">
+              </h4>
+              <p className="lg:font-light">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
                 massa dolor, fermentum vitae ante eget, eleifend fermentum orci.
                 Morbi accumsan nisl quis leo lobortis porttitor. Vestibulum
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -17,50 +17,60 @@ const Contact = () => {
   return (
     <section
       id="contato"
-      className="flex h-screen items-center justify-center bg-blue-950"
+      className="bg-gradient flex h-full items-center justify-center bg-gradient-to-r from-[#185083] to-[#0FA3E3] px-4 py-8 lg:px-40 lg:py-24"
     >
       <div id="contato-div" className="max-7xl">
         <div
           id="contato-content"
-          className="flex w-full flex-col items-center justify-between gap-8 px-4 md:flex-row md:gap-80 md:px-0"
+          className="flex w-full flex-col items-center justify-between gap-8 lg:flex-row lg:gap-36"
         >
           <div
             id="contato-texto"
-            className="flex max-w-md flex-col gap-16 text-white"
+            className="flex max-w-md flex-col gap-6 text-[#DCF4FF] lg:w-[527px] lg:gap-12"
           >
-            <div className="space-y-4">
-              <h2 className="text-4xl font-semibold">
-                Entre em contato conosco!
-              </h2>
-              <p className="font-light">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Laborum, itaque!
-              </p>
+            <div id="texto-parte1" className="flex flex-col gap-4">
+              <h5 className="text-2xl uppercase leading-8">Contato</h5>
+
+              <div className="flex flex-col gap-4 lg:gap-6">
+                <h4 className="text-4xl leading-[49px]">
+                  Vamos trabalhar juntos.
+                </h4>
+                <p className="leading-5">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aliquam massa dolor, fermentum vitae ante eget, eleifend
+                  fermentum orci.
+                </p>
+              </div>
             </div>
-            <div className="flex flex-col gap-8 font-medium">
-              <div className="flex items-center justify-start gap-4">
+
+            <div id="texto-parte2" className="flex flex-col gap-5">
+              <div className="flex items-center justify-start gap-3">
                 <FontAwesomeIcon icon={faPhone} size="lg" />
                 <span>35 12345-6789</span>
               </div>
-              <div className="flex items-center justify-start gap-4">
+              <div className="flex items-center justify-start gap-3">
                 <FontAwesomeIcon icon={faEnvelope} size="lg" />
                 <span>contato@empresa.com</span>
               </div>
-              <div className="flex items-center justify-start gap-4">
+              <div className="flex items-center justify-start gap-3">
                 <FontAwesomeIcon icon={faLocationDot} size="lg" />
                 <span>
                   Av. BPS, 1303 - Pinheirinho, Itajubá - MG, 37500-903
                 </span>
               </div>
             </div>
-            <div className="flex gap-4">
+
+            <div className="flex gap-8 lg:gap-4">
               <Icon link="https://www.instagram.com/" iconName={faInstagram} />
               <Icon link="https://www.facebook.com/" iconName={faFacebook} />
               <Icon link="https://www.linkedin.com/" iconName={faLinkedinIn} />
             </div>
           </div>
 
-          <div id="contato-form" className="rounded-2xl bg-white">
+          <div
+            id="contato-form"
+            className="w-full rounded-lg bg-white lg:w-[450px]"
+          >
             <Form />
           </div>
         </div>
